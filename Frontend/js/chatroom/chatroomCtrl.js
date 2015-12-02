96a2a5b5550faf5192a9563c2196883d1d3fa866
J. Tanner Porter <jtporter9@gmail.com>
2015-12-02 09:35:25 -0700

    working Oauth facebook

--- Frontend/js/chatroom/chatroomCtrl.js
+++ Frontend/js/chatroom/chatroomCtrl.js
@@ -1,294 +1,264 @@
-angular.module('messangerApp')
+angular.module('messangerApp').controller('chatroomCtrl', function ($scope, chatroomService, $location, $anchorScroll) {
+	/////scrolly stuff///////
+	
+	// $scope.im = {};
 
-	// .directive('schrollBottom', function () {
-	// 	return {
-	// 		scope: {
-	// 			schrollBottom: "="
-	// 		},
-	// 		link: function (scope, element) {
-	// 			scope.$watchCollection('schrollBottom', function (newValue) {
-	// 				if (newValue) {
-	// 					$(element).scrollTop($(element)[0].scrollHeight);
-	// 				}
-	// 			});
-	// 		}
-	// 	}
-	// })
+	// $scope.sendIM = function (msg) {
 
+	// 	$scope.messages.push(msg);
+	// 	$scope.im = {};
 
-	.controller('chatroomCtrl', function ($scope, chatroomService, $location, $anchorScroll) {
-		// $scope.conversations = chatroomService.conversations;
+	// 	$('#chatBox').scrollTop($('#chatBox')[0].scrollHeight);
+
+	// 	var chatBox = document.getElementById('chatBox');
+	// 	chatBox.scrollTop = 300 + 8 + ($scope.messages.length * 240);
+	// }
+	
+	/////////////////REAL STUFF//////////////////////
+
+	$scope.showSignuature = false;
+	$scope.showProfileLink = true;
+	$scope.showChatroomLink = false;
+	$scope.chatroomTitle = true;
+	$scope.profileTitle = false;
+
+	$scope.showlittleInput = true;
+	
+	//adding conversation
+	$scope.addConversation = function () {
+		$scope.addingConversation = true;
+	};
+
+	$scope.submitNewConvo = function () {
+		$scope.addingConversation = false;
+		$scope.scrollFriendsFinder = "";
+	}
+
+	$scope.cancelForm = function () {
+		$scope.addingConversation = false;
+		$scope.scrollFriendsFinder = "";
+	}
 	
 	
-		/////scrolly stuff///////
-	
-		$scope.im = {};
-
-		$scope.sendIM = function (msg) {
-
-			$scope.messages.push(msg);
-			$scope.im = {};
-
-			$('#chatBox').scrollTop($('#chatBox')[0].scrollHeight);
-
-			var chatBox = document.getElementById('chatBox');
-			chatBox.scrollTop = 300 + 8 + ($scope.messages.length * 240);
-		}
-	
-		/////////////////REAL STUFF//////////////////////
-		$scope.showSignuature = false;
-		$scope.showProfileLink = true;
-		$scope.showChatroomLink = false;
-		$scope.chatroomTitle = true;
-		$scope.profileTitle = false;
-
+	//sending new message area
+	$scope.sendNewMessage = function (i) {
 		$scope.showlittleInput = true;
-
-		$scope.showLargeInputBox = function () {
-			$scope.showlittleInput = false;
-			$scope.showLargeInput = true;
-			$scope.newMessageContainer = 'newMessage-container';
+		$scope.showLargeInput = false;
+		$scope.newMessageText = "";
+		$scope.showFileUpload = false;
+		var newMessage = {
+			from: 'You',
+			content: i,
+			time: new Date
 		}
-	
-		//adding conversation
-		$scope.addConversation = function () {
-			$scope.addingConversation = true;
-		};
+		$scope.messages.push(newMessage);
+		$('#message-container').scrollTop($('#message-container')[0].scrollHeight + 200);
+		
+		
+		// var chatBox = document.getElementById('#message-container');
+		// chatBox.scrollTop = 300 + 8 + ($scope.messages.length * 240);
+	}
 
-		$scope.submitNewConvo = function () {
-			$scope.addingConversation = false;
-			$scope.scrollFriendsFinder = "";
+	$scope.attachFile = function () {
+		$scope.showFileUpload = !$scope.showFileUpload;
+	}
+	$scope.searchKeyPress = function (e, i) {
+		e = e || window.event;
+		if (e.keyCode == 13) {
+			$scope.sendNewMessage(i);
+			return false;
 		}
+		return true;
+	}
 
-		$scope.cancelForm = function () {
-			$scope.addingConversation = false;
-			$scope.scrollFriendsFinder = "";
+
+
+
+	$scope.messages = [
+		{
+			from: 'Freddie Davis',
+			content: 'Dude, Tanner are you coming tonight?',
+			time: new Date
+		},
+		{
+			from: 'Zac',
+			content: 'I am, yeah',
+			time: new Date
+		},
+		{
+			from: 'You',
+			content: 'Yeah I can be there',
+			time: new Date,
+			id: 1
+		},
+		{
+			from: 'Zac',
+			content: 'Actually my wife wont let me',
+			time: new Date
+		},
+		{
+			from: 'Taylor Rowley',
+			content: 'I can\'t',
+			time: new Date
+		},
+		{
+			from: 'You',
+			content: 'Dude what!?',
+			time: new Date,
+			id: 1
+		},
+		{
+			from: 'You',
+			content: 'It\'s okay I\'m just kidding',
+			time: new Date,
+			id: 1
+		},
+		{
+			from: 'Taylor Rowley',
+			content: 'Haha okay.',
+			time: new Date
+		},
+	];
+
+	$scope.conversations = [
+		{
+			from: ['Freddie Davis', 'Zac Christianson', 'Taylor Rowley'],
+			date: new Date,
+			messageCount: 33,
+		},
+		{
+			from: ['Tobee Gunter'],
+			date: new Date,
+			messageCount: 54
+		}, {
+			from: ['Mom', 'Daddy'],
+			date: new Date,
+			messageCount: 21
+		}, {
+			from: ['Coach Burbidge'],
+			date: new Date,
+			messageCount: 11
+		}, {
+			from: ['Rey'],
+			date: new Date,
+			messageCount: 82
+		}, {
+			from: ['Zac'],
+			date: new Date,
+			messageCount: 12
+		}, {
+			from: ['Luke Skywalker'],
+			date: new Date,
+			messageCount: 81
+		},
+
+	];
+	$scope.friends = [
+		{
+			name: 'Freddie Davis',
+			email: 'fpdavis4@gmail.com',
+			status: 'Offline'
+		},
+		{
+			name: 'Zac Christianson',
+			email: 'zaclax29@gmail.com',
+			status: 'Online'
+		}, {
+			name: 'Tobee Gunter',
+			email: 'sobee4@gmail.com',
+			status: 'Online'
+		}, {
+			name: 'Pete Bradford',
+			email: 'peter26@gmail.com',
+			status: 'Offline'
+		}, {
+			name: 'Cody Porter',
+			email: 'codyWadePorter@gmail.com',
+			status: 'Online'
+		}, {
+			name: 'Freddie Davis',
+			email: 'fpdavis4@gmail.com',
+			status: 'Offline'
+		},
+		{
+			name: 'Zac Christianson',
+			email: 'zaclax29@gmail.com',
+			status: 'Online'
+		}, {
+			name: 'Tobee Gunter',
+			email: 'sobee4@gmail.com',
+			status: 'Online'
+		}, {
+			name: 'Pete Bradford',
+			email: 'peter26@gmail.com',
+			status: 'Offline'
+		}, {
+			name: 'Cody Porter',
+			email: 'codyWadePorter@gmail.com',
+			status: 'Online'
+		}, {
+			name: 'Freddie Davis',
+			email: 'fpdavis4@gmail.com',
+			status: 'Offline'
+		},
+		{
+			name: 'Zac Christianson',
+			email: 'zaclax29@gmail.com',
+			status: 'Online'
+		}, {
+			name: 'Tobee Gunter',
+			email: 'sobee4@gmail.com',
+			status: 'Online'
+		}, {
+			name: 'Pete Bradford',
+			email: 'peter26@gmail.com',
+			status: 'Offline'
+		}, {
+			name: 'Cody Porter',
+			email: 'codyWadePorter@gmail.com',
+			status: 'Online'
+		}, {
+			name: 'Freddie Davis',
+			email: 'fpdavis4@gmail.com',
+			status: 'Offline'
+		},
+		{
+			name: 'Zac Christianson',
+			email: 'zaclax29@gmail.com',
+			status: 'Online'
+		}, {
+			name: 'Tobee Gunter',
+			email: 'sobee4@gmail.com',
+			status: 'Online'
+		}, {
+			name: 'Pete Bradford',
+			email: 'peter26@gmail.com',
+			status: 'Offline'
+		}, {
+			name: 'Cody Porter',
+			email: 'codyWadePorter@gmail.com',
+			status: 'Online'
+		}, {
+			name: 'Freddie Davis',
+			email: 'fpdavis4@gmail.com',
+			status: 'Offline'
+		},
+		{
+			name: 'Zac Christianson',
+			email: 'zaclax29@gmail.com',
+			status: 'Online'
+		}, {
+			name: 'Tobee Gunter',
+			email: 'sobee4@gmail.com',
+			status: 'Online'
+		}, {
+			name: 'Pete Bradford',
+			email: 'peter26@gmail.com',
+			status: 'Offline'
+		}, {
+			name: 'Cody Porter',
+			email: 'codyWadePorter@gmail.com',
+			status: 'Online'
 		}
-	
-	
-		//sending new message area
-		$scope.sendNewMessage = function (i) {
-			$scope.showlittleInput = true;
-			$scope.showLargeInput = false;
-			$scope.newMessageText = "";
-			$scope.newMessageContainer = 'message-container';
-			$scope.showFileUpload = false;
-			var newMessage = {
-				from: 'You',
-				content: i,
-				time: new Date
-			}
-			$scope.messages.push(newMessage);
-			var chatbox = $('#message-container');
-			var height = chatbox[0].scrollHeight - chatbox.height();
-			chatbox.scrollTop(height + 200);
-			// $('#message-container').scrollTop($('#message-container')[0].scrollHeight + 200);
-
-			// 	var chatBox = document.getElementById('message-container');
-			// 	chatBox.scrollTop = 400 + 10 + ($scope.messages.length * 260);
-		}
-
-		$scope.attachFile = function () {
-			$scope.showFileUpload = !$scope.showFileUpload;
-		}
-		$scope.searchKeyPress = function (e, i) {
-			// look for window.event in case event isn't passed in
-			e = e || window.event;
-			if (e.keyCode == 13) {
-				$scope.sendNewMessage(i);
-				return false;
-			}
-			return true;
-		}
-
-
-
-
-		$scope.messages = [
-			{
-				from: 'Freddie Davis',
-				content: 'Dude, Tanner are you coming tonight?',
-				time: new Date
-			},
-			{
-				from: 'Zac',
-				content: 'I am, yeah',
-				time: new Date
-			},
-			{
-				from: 'You',
-				content: 'Yeah I can be there',
-				time: new Date,
-				id: 1
-			},
-			{
-				from: 'Zac',
-				content: 'Actually my wife wont let me',
-				time: new Date
-			},
-			{
-				from: 'Taylor Rowley',
-				content: 'I can\'t',
-				time: new Date
-			},
-			{
-				from: 'You',
-				content: 'Dude what!?',
-				time: new Date,
-				id: 1
-			},
-			{
-				from: 'You',
-				content: 'It\'s okay I\'m just kidding',
-				time: new Date,
-				id: 1
-			},
-			{
-				from: 'Taylor Rowley',
-				content: 'Haha okay.',
-				time: new Date
-			},
-		];
-
-		$scope.conversations = [
-			{
-				from: ['Freddie Davis', 'Zac Christianson', 'Taylor Rowley'],
-				date: new Date,
-				messageCount: 33,
-			},
-			{
-				from: ['Tobee Gunter'],
-				date: new Date,
-				messageCount: 54
-			}, {
-				from: ['Mom', 'Daddy'],
-				date: new Date,
-				messageCount: 21
-			}, {
-				from: ['Coach Burbidge'],
-				date: new Date,
-				messageCount: 11
-			}, {
-				from: ['Rey'],
-				date: new Date,
-				messageCount: 82
-			}, {
-				from: ['Zac'],
-				date: new Date,
-				messageCount: 12
-			}, {
-				from: ['Luke Skywalker'],
-				date: new Date,
-				messageCount: 81
-			},
-
-		];
-		$scope.friends = [
-			{
-				name: 'Freddie Davis',
-				email: 'fpdavis4@gmail.com',
-				status: 'Offline'
-			},
-			{
-				name: 'Zac Christianson',
-				email: 'zaclax29@gmail.com',
-				status: 'Online'
-			}, {
-				name: 'Tobee Gunter',
-				email: 'sobee4@gmail.com',
-				status: 'Online'
-			}, {
-				name: 'Pete Bradford',
-				email: 'peter26@gmail.com',
-				status: 'Offline'
-			}, {
-				name: 'Cody Porter',
-				email: 'codyWadePorter@gmail.com',
-				status: 'Online'
-			}, {
-				name: 'Freddie Davis',
-				email: 'fpdavis4@gmail.com',
-				status: 'Offline'
-			},
-			{
-				name: 'Zac Christianson',
-				email: 'zaclax29@gmail.com',
-				status: 'Online'
-			}, {
-				name: 'Tobee Gunter',
-				email: 'sobee4@gmail.com',
-				status: 'Online'
-			}, {
-				name: 'Pete Bradford',
-				email: 'peter26@gmail.com',
-				status: 'Offline'
-			}, {
-				name: 'Cody Porter',
-				email: 'codyWadePorter@gmail.com',
-				status: 'Online'
-			}, {
-				name: 'Freddie Davis',
-				email: 'fpdavis4@gmail.com',
-				status: 'Offline'
-			},
-			{
-				name: 'Zac Christianson',
-				email: 'zaclax29@gmail.com',
-				status: 'Online'
-			}, {
-				name: 'Tobee Gunter',
-				email: 'sobee4@gmail.com',
-				status: 'Online'
-			}, {
-				name: 'Pete Bradford',
-				email: 'peter26@gmail.com',
-				status: 'Offline'
-			}, {
-				name: 'Cody Porter',
-				email: 'codyWadePorter@gmail.com',
-				status: 'Online'
-			}, {
-				name: 'Freddie Davis',
-				email: 'fpdavis4@gmail.com',
-				status: 'Offline'
-			},
-			{
-				name: 'Zac Christianson',
-				email: 'zaclax29@gmail.com',
-				status: 'Online'
-			}, {
-				name: 'Tobee Gunter',
-				email: 'sobee4@gmail.com',
-				status: 'Online'
-			}, {
-				name: 'Pete Bradford',
-				email: 'peter26@gmail.com',
-				status: 'Offline'
-			}, {
-				name: 'Cody Porter',
-				email: 'codyWadePorter@gmail.com',
-				status: 'Online'
-			}, {
-				name: 'Freddie Davis',
-				email: 'fpdavis4@gmail.com',
-				status: 'Offline'
-			},
-			{
-				name: 'Zac Christianson',
-				email: 'zaclax29@gmail.com',
-				status: 'Online'
-			}, {
-				name: 'Tobee Gunter',
-				email: 'sobee4@gmail.com',
-				status: 'Online'
-			}, {
-				name: 'Pete Bradford',
-				email: 'peter26@gmail.com',
-				status: 'Offline'
-			}, {
-				name: 'Cody Porter',
-				email: 'codyWadePorter@gmail.com',
-				status: 'Online'
-			}
-		];
-	});
+	];
+});
--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+
+///// Passport Oauth////////
+var session = require('express-session');
+var passport = require('passport');
+var FacebookStrategy = require('passport-facebook').Strategy;
+var GoogleStrategy = require('passport-google-oauth').OAuthStrategy;
 // Dependencies
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -5,10 +11,53 @@ var cors = require('cors');
 var mongoose = require('mongoose');
 var app = express();
 
+var keys = require('./keys');
+
+app.use(session({ secret: 'IgotapickleIgotapickleheyheyhey' }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new FacebookStrategy({
+	clientID: keys.facebookId,
+	clientSecret: keys.facebookSecret,
+	callbackURL: 'http://localhost:3000/auth/facebook/callback'
+}, function (token, refreshToken, profile, done) {
+	return done(null, profile);
+}));
+
+///////Facebook Oauth endpoints//////////
+app.get('/auth/facebook', passport.authenticate('facebook'));
+app.get('/auth/facebook/callback', passport.authenticate('facebook', {
+	successRedirect: '/',
+	failureRedirect: '/login'
+}), function (req, res) {
+	console.log(req.session);
+});
+
+passport.serializeUser(function (user, done) {
+	done(null, user);
+});
+
+var requireAuth = function (req, res, next) {
+	if (req.isAuthenticated()) {
+		return next();
+	}
+	return res.redirect('/auth/facebook')
+}
+
+passport.deserializeUser(function (obj, done) {
+	done(null, obj);
+});
+
+app.get('/me', requireAuth, function (req, res, next) {
+	var currentLoggedInUserOnSession = req.user;
+	res.send(currentLoggedInUserOnSession);
+})
+
 //Mongoose
 var mongoUri = "mongodb://localhost:27017/Chatroom";
 
-// Controllers
 
 
 // Express Middleware
@@ -26,6 +75,6 @@ app.use(express.static(__dirname + '/Frontend'));
 // Endpoints
 
 //listening
-app.listen(3000, function() {
-  console.log('listening on port: 3000');
+app.listen(3000, function () {
+	console.log('listening on port: 3000');
 });
