85625d961f11646dabda8ccb014b2ec9614aa31b
J. Tanner Porter <jtporter9@gmail.com>
2015-12-14 15:18:21 -0700

    bug

--- Frontend/js/chatroom/chatroomCtrl.js
+++ Frontend/js/chatroom/chatroomCtrl.js
@@ -51,7 +51,7 @@ angular.module('messangerApp').controller('chatroomCtrl', function ($scope,
 	})
 
 
-	$scope.findCurrentUserById($scope.usersInfo._id);
+	$scope.findCurrentUserById($stateParams.id);
 	
 	//////////////////////////////
 	////////////Emojis///////////
